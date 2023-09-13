9cc0c1d970064309fce02cb86118a6a2c02a4ec0
sekharmisra <59437658+sekharmisra@users.noreply.github.com>
2023-09-13 04:44:16 -0400

    Commited if else and arrays!

--- 03-hello-conditions/ifelse.js
+++ 03-hello-conditions/ifelse.js
@@ -2,5 +2,21 @@ let age = 18
 if (age < 18){
     console.log("Not eligibile for driving license")
 }else{
-    console.log("Eligible for drivinig license!")
+    console.log("Eligible for driving license!")
+}
+
+if (age == "18"){
+    console.log("== Bug in Javascript: Eligible for driving license!")
+}
+
+if (age === "18"){
+    console.log("Eligible for driving license!")
+}
+
+if (age === 18){
+    console.log("Eligible for driving license!")
+}
+
+if (age !== 20){
+    console.log("Not equal!")
 }
---
+++ 04-hello-arrays/array1.js
@@ -0,0 +1,13 @@
+const names = ["sekhar", "Kunjan", "Aarna"]
+console.log(names)
+console.log(names.length)
+names.push("Baby")
+console.log(names)
+console.log(names.length)
+
+const firstname = "Sekhar Misra"
+console.log(firstname[9])
+console.log(firstname.length)
+
+const arrayfirstname = Array.from(firstname)
+console.log(arrayfirstname.length)
